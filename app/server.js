82a81c8c86c654f55b8af193beb198397012e297
Ming-der Wang <ming@log4analytics.com>
2016-06-07 11:10:42 +0800

    ok for review

--- app/server.js
+++ app/server.js
@@ -4,11 +4,6 @@ var app = express();
 // static content
 app.use(express.static(__dirname));
 
-// let pushState and the router do the navigation. return index.html for all non-static-content routes.
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log('server running on port ' + port);
